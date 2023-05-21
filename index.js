679e267448b6150a121dde4e8ffc77c794fe6ced
ebadulhassan <ebadulhassan967@gmail.com>
2023-05-21 15:31:37 -0700

    2assigment

---
+++ index.js
@@ -0,0 +1,20 @@
+// let value1 = prompt("Set the price of chicken");
+// let value2 = prompt("how much do you need chicken");
+
+// value1/1000
+// let value3 = 0.7*value1
+
+// document.write("you enter 1 kg price of ",value1,"</br>");
+// document.write(value2," me ai ga ",value1,"gram")
+
+
+
+
+let value1 = prompt("Set The Price Of Chicken")
+value1/1000
+
+let value2 = 0.7*value1
+let value3 = prompt("how much do you neeed")
+
+document.write("you set the price of chicken is ",value1,"prkg","</br>")
+document.write(value3,"me ai ga ",value2,"Gram")
